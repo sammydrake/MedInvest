4b4cffd3f469fde3f52d7ec0034c9688eb37b878
sammydrake <drakesammy0101@gmail.com>
2023-03-28 20:44:24 -0500

    new

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 bg-blue-200">
